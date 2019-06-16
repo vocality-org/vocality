d49a77d07dee54dd3742c90b9896e862819ebdcd
MatthiasHerzog2000 <gfmerwin@gmail.com>
2019-06-16 18:44:17 +0200

    working help, play and changeprefix command

--- src/classes/ShardingController.ts
+++ src/classes/ShardingController.ts
@@ -1,19 +1,19 @@
-import { ShardingManager } from 'discord.js';
-import { config } from '../config';
+import { ShardingManager } from "discord.js";
+import { config } from "../config";
 
 export class ShardingController {
-    shardingManager: ShardingManager;
-    
-    constructor() {
-        this.shardingManager = new ShardingManager('./bot.ts', {
-            totalShards: config.SHARDS,
-            token: config.TOKEN,
-        })
-    }
+  shardingManager: ShardingManager;
 
-    start() {
-        this.shardingManager.spawn();
-    }
+  constructor() {
+    this.shardingManager = new ShardingManager(__dirname + "/../bot.ts", {
+      totalShards: config.SHARDS,
+      token: config.TOKEN
+    });
+  }
+
+  start() {
+    this.shardingManager.spawn();
+  }
 }
 
 //? https://github.com/discordjs/guide/tree/master/guide/sharding
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const config = {
-    PREFIX: '?',
-    TOKEN: 'NTg5NTk1MTg5NjMxMzg1NjAy.XQWd1w.5yZcJNlE3Hs5sRqePdXX0sBvH6Y',
-    SHARDS: 1
+  PREFIX: "?",
+  SERVERPREFIXES: {} as any,
+  TOKEN: "NTg5NTk1MTg5NjMxMzg1NjAy.XQWd1w.5yZcJNlE3Hs5sRqePdXX0sBvH6Y",
+  SHARDS: 1
 };
