e7daecdc1104bdf5c8e14b4190bb4660e2a5fee5
MatthiasHerzog2000 <gfmerwin@gmail.com>
2019-06-17 00:01:23 +0200

    adding commands to bot

--- src/classes/BotClient.ts
+++ src/classes/BotClient.ts
@@ -1,13 +1,14 @@
 import { Client as DiscordClient, ClientOptions } from "discord.js";
+import * as Discord from "discord.js";
 import { config } from "../config";
 
 export class BotClient extends DiscordClient {
   initTime: number;
+  commands = new Discord.Collection<string, any>();
 
   constructor(options?: ClientOptions | undefined) {
     super(options ? options : undefined);
     this.initTime = Date.now();
-
     this.once("ready", () => {
       this.user.setActivity(`${config.PREFIX}help`);
       const allGuilds = this.guilds;
