1790907ad47108b2d82434721473a5c9b4a83f93
Julian Kaindl <kaindl745@gmail.com>
2019-06-28 14:08:54 +0200

    Move creation of QueueContracts into Controller

--- src/commands/Skip.ts
+++ src/commands/Skip.ts
@@ -8,7 +8,7 @@ export class Skip implements Command {
     const serverEntry = ServerQueueController.getInstance().find(msg.guild.id)!;
     if (!msg.member.voiceChannel)
       msg.channel.send("You have to be in a voice channel to stop the music!");
-    else if (serverEntry.songs.length === 0)
+    else if (!serverEntry)
       msg.channel.send("There is no song that I could skip!");
     else if (args[0]) {
       if (Number(args[0])) {
