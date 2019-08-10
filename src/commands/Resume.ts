a106ab280211d7a96dafcb2cdfca48bf172ab4e4
MatthiasHerzog2000 <gfmerwin@gmail.com>
2019-08-10 11:39:43 +0200

    pause and resume now working

--- src/commands/Resume.ts
+++ src/commands/Resume.ts
@@ -1,7 +1,18 @@
-import { Command } from '../interfaces/Command';
+import { Command } from "../interfaces/Command";
+import { Message } from "discord.js";
+import { ServerQueueController } from "../classes/ServerQueueController";
 
 export class Resume implements Command {
-    execute (): void {
-        throw new Error('Method not implemented.');
+  execute(msg: Message, args: string[]): void {
+    if (ServerQueueController.getInstance().find(msg.guild.id) === undefined)
+      return;
+    const serverEntry = ServerQueueController.getInstance().find(msg.guild.id)!;
+    if (serverEntry.songs.length === 0) return;
+    if (!serverEntry.connection!.dispatcher.paused) {
+      msg.channel.send("Nothing to Resume");
+      return;
     }
+    serverEntry.connection!.dispatcher.resume();
+    msg.channel.send(`${serverEntry.songs[0].title} resumed`);
+  }
 }
