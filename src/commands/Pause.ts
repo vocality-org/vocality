cd1d047a672b7bb8c58f98c286e13e6a6dcddb8b
MatthiasHerzog2000 <gfmerwin@gmail.com>
2019-08-10 11:39:43 +0200

    pause and resume now working

--- src/commands/Pause.ts
+++ src/commands/Pause.ts
@@ -1,7 +1,14 @@
-import { Command } from '../interfaces/Command';
+import { Command } from "../interfaces/Command";
+import { Message } from "discord.js";
+import { ServerQueueController } from "../classes/ServerQueueController";
 
 export class Pause implements Command {
-  execute (): void {
-    throw new Error('Method not implemented.');
+  execute(msg: Message, args: string[]): void {
+    if (ServerQueueController.getInstance().find(msg.guild.id) === undefined)
+      return;
+
+    const connection = ServerQueueController.getInstance().find(msg.guild.id)!
+      .connection;
+    connection!.dispatcher.pause();
   }
 }
