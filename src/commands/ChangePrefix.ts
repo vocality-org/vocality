7ba16a88bce40bb668c080d653b0c907470802d4
Julian Kaindl <kaindl745@gmail.com>
2019-09-02 14:10:07 +0200

    Remove Decorator because other than being cute its useless
    CommandOptions still exist.
    In the form of an optional property on the command interface

--- src/commands/ChangePrefix.ts
+++ src/commands/ChangePrefix.ts
@@ -1,18 +1,17 @@
 import { Message } from 'discord.js';
 import { Command } from '../interfaces/Command';
 import { Bot } from '../config';
-import { InitCommand } from '../decorators/InitCommand';
+import { CommandOptions } from '../interfaces/CommandOptions';
 
-@InitCommand({
-  name: 'changePrefix',
-  description: 'Change the bots prefix',
-  hasArguments: true
-})
 export class ChangePrefix implements Command {
-  execute(msg: Message, args: string[]): void {
-    Bot.SERVERPREFIXES[msg.guild.id] = args[0];
-    msg.channel.send(
-      `Prefix changed to \`${Bot.SERVERPREFIXES[msg.guild.id]}\``
-    );
-  }
+    options: CommandOptions = {
+        name: 'changePrefix',
+        description: 'Change the bots prefix',
+        hasArguments: true,
+    };
+
+    execute(msg: Message, args: string[]): void {
+        Bot.SERVERPREFIXES[msg.guild.id] = args[0];
+        msg.channel.send(`Prefix changed to \`${Bot.SERVERPREFIXES[msg.guild.id]}\``);
+    }
 }
