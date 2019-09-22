9077b38eabea1197dbec26a8b66e9809f31eed00
MatthiasHerzog2000 <gfmerwin@gmail.com>
2019-09-22 16:45:54 +0200

    enable socket secure connection

--- src/dashboard-ws.ts
+++ src/dashboard-ws.ts
@@ -1,4 +1,11 @@
 import io from 'socket.io';
+import fetch from 'node-fetch';
 const socketio = io();
+socketio.use(async (socket) => {
+    const response = await fetch('https://www.discordapp.com/api/users/@me', { headers: {'Authorization': `Bearer ${socket.handshake.query.discordKey}`}})
+    if(response.status != 200) {
+        socket.disconnect();
+    }
+})
 socketio.on('connection', client => { console.log("connected") });
 socketio.listen(3000);
