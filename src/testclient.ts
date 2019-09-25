bf2cb7f870c6160ade2ff6cada875d1f82267f7e
MatthiasHerzog2000 <gfmerwin@gmail.com>
2019-09-25 15:54:06 +0200

    change socket origins

--- src/testclient.ts
+++ src/testclient.ts
@@ -1,2 +1,2 @@
 import io from 'socket.io-client'
-const socket = io('http://localhost:3000', {query: 'discordKey=asdfasdf'});
+const socket = io('https://bass-bot-app.herokuapp.com/', {query: 'discordKey=asdfasdf'});
