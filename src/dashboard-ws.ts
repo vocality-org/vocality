c41231e055dcf4d9a39bd310945573c2015ae316
MatthiasHerzog2000 <gfmerwin@gmail.com>
2019-09-25 15:54:06 +0200

    change socket origins

--- src/dashboard-ws.ts
+++ src/dashboard-ws.ts
@@ -1,6 +1,7 @@
 import io from 'socket.io';
 import fetch from 'node-fetch';
 const socketio = io();
+socketio.origins('*:*');
 socketio.use(async (socket) => {
     const response = await fetch('https://www.discordapp.com/api/users/@me', { headers: {'Authorization': `Bearer ${socket.handshake.query.discordKey}`}})
     if(response.status != 200) {
