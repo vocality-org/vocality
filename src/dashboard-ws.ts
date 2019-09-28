58d1af0cfb20c198779868143fd44770961fe753
MatthiasHerzog2000 <gfmerwin@gmail.com>
2019-09-28 16:32:20 +0200

    add event to send guilds

--- src/dashboard-ws.ts
+++ src/dashboard-ws.ts
@@ -4,6 +4,7 @@ import { ServerQueueController } from './classes/ServerQueueController';
 import { BotClient } from './classes/BotClient';
 import { MessageHandler } from './classes/MessageHandler';
 import { bot } from './bot';
+import { Guild } from 'discord.js';
 const socketio = io.listen(process.env.PORT || 3000);
 socketio.origins('*:*');
 socketio.use((socket, next) => {
@@ -16,16 +17,18 @@ socketio.use((socket, next) => {
     })
  })
 socketio.on("connection", function(socket) {
-    if(bot.user) {
-        socketio.emit('botid', bot.user.id);
-    }
-    
-    socketio.on('currentGuild', (guildId: string) => {
+    socket.on('userGuilds', (guilds: string[]) => {
+        let serverEntries = ServerQueueController.getInstance().getAll();
+        let ids = Array.from(serverEntries.keys())
+        let sameIds = guilds.filter(gId => ids.includes(gId));
+        socket.emit('botGuilds', sameIds);
+    })
+    socket.on('currentGuild', (guildId: string) => {
         const serverEntry = ServerQueueController.getInstance().find(guildId)!!;
         if(serverEntry) {
             const currentSong = serverEntry.songs[0];
-            socketio.emit('currentQueue', serverEntry.songs);
-            socketio.emit('currentSong', {title: currentSong.title, thumbnail_url: currentSong.thumbnail_url, requested_by: currentSong.requested_by, max_time_ms: currentSong.length_ms, current_time_ms: serverEntry.connection!!.dispatcher.time})
+            socket.emit('currentQueue', serverEntry.songs);
+            socket.emit('currentSong', {title: currentSong.title, thumbnail_url: currentSong.thumbnail_url, requested_by: currentSong.requested_by, max_time_ms: currentSong.length_ms, current_time_ms: serverEntry.connection!!.dispatcher.time})
         }
         
         
