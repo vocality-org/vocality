4168196d8ef39ed4146c4c2b37264450c1d9f97d
KaindlJulian <kaindl745@gmail.com>
2019-11-02 16:32:15 +0100

    test(core): Add test for hasArguments: false

--- test/core/ArgumentParser.test.ts
+++ test/core/ArgumentParser.test.ts
@@ -16,7 +16,7 @@ describe('ArgumentParser', () => {
     });
   });
 
-  describe('validateArguments', () => {
+  describe('.validateArguments()', () => {
     it('should throw error when command has arguments but none are provided', () => {
       const commandWithArgs: Command = {
         options: {
@@ -32,21 +32,37 @@ describe('ArgumentParser', () => {
         BotError
       );
     });
-  });
 
-  it('should not throw error when command does not have arguments', () => {
-    const commandWithArgs: Command = {
-      options: {
-        name: '',
-        description: '',
-        socketEnabled: false,
-        hasArguments: true,
-      },
-      execute: () => {},
-    };
-    assert.doesNotThrow(
-      () => ArgumentParser.validateArguments(commandWithArgs, ['arg1']),
-      BotError
-    );
+    it('should not throw error when command has arguments and at least one is provided', () => {
+      const commandWithArgs: Command = {
+        options: {
+          name: '',
+          description: '',
+          socketEnabled: false,
+          hasArguments: true,
+        },
+        execute: () => {},
+      };
+      assert.doesNotThrow(
+        () => ArgumentParser.validateArguments(commandWithArgs, ['arg1']),
+        BotError
+      );
+    });
+
+    it('should ignore any arguments when command has no arguments', () => {
+      const commandWithoutArgs: Command = {
+        options: {
+          name: '',
+          description: '',
+          socketEnabled: false,
+          hasArguments: false,
+        },
+        execute: () => {},
+      };
+      assert.doesNotThrow(
+        () => ArgumentParser.validateArguments(commandWithoutArgs, ['arg1']),
+        BotError
+      );
+    });
   });
 });
