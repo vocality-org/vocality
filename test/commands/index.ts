ee3a8b589a16ff4b90696cff09b32a0bed52dfb7
KaindlJulian <kaindl745@gmail.com>
2019-11-09 02:59:08 +0100

    test(general): Add test suite for commands

---
+++ test/commands/index.ts
@@ -0,0 +1,40 @@
+import { ResponseClient, ResponseClientOptions } from 'discord-response-mock';
+import { BOT } from '../../src/config';
+import { DISCORD_TEST } from './config';
+
+import { current } from './Current.test';
+import { lyrics } from './Lyrics.test';
+import { skip } from './Skip.test';
+
+const options: ResponseClientOptions = {
+  channelId: DISCORD_TEST.TEST_CHANNEL_ID,
+  messagePrefix: BOT.PREFIX,
+  specificUserId: '589595189631385602', // vocality bot id
+  responseTimeout: 4000,
+};
+
+export let client = new ResponseClient(options);
+
+describe('commands', function() {
+  this.timeout(options.responseTimeout!);
+
+  before(async () => {
+    client = await client.setup(DISCORD_TEST.BOT_TOKEN, DISCORD_TEST.GUILD_ID);
+  });
+
+  describe('current', () => {
+    current.call(null, client);
+  });
+
+  describe('lyrics', () => {
+    lyrics.call(null, client);
+  });
+
+  describe('skip', () => {
+    skip.call(null, client);
+  });
+
+  after(async () => {
+    await client.cleanup();
+  });
+});
