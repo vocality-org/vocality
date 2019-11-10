fcf805d1a3a4541e5814f8655600efc262a8606c
KaindlJulian <kaindl745@gmail.com>
2019-11-10 14:03:09 +0100

    test(loop): Initial loop command enables the loop

--- test/commands/Loop.test.ts
+++ test/commands/Loop.test.ts
@@ -1,14 +1,11 @@
-import { ResponseClient } from "discord-response-mock";
-import * as assert from "assert";
+import { ResponseClient } from 'discord-response-mock';
+import * as assert from 'assert';
 
 export const loop = (client: ResponseClient) => {
-    it('It should notify the user that loop is now enabled', done => {
-      client.write('loop', response => {
-        assert.equal(
-          response.content,
-          'Repeating is now disabled'
-        );
-        done();
-      });
+  it('It should notify the user that loop is now enabled', done => {
+    client.write('loop', response => {
+      assert.equal(response.content, 'Repeating is now `enabled`');
+      done();
     });
-  };
+  });
+};
