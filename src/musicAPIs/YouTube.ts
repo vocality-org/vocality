5a4a7b4c6294c739e43fe49c293389af8636a046
MatthiasHerzog2000 <gfmerwin@gmail.com>
2019-11-16 13:11:33 +0100

    feat(general): Autoplay works now with youtube recommended videos

--- src/musicAPIs/YouTube.ts
+++ src/musicAPIs/YouTube.ts
@@ -122,4 +122,18 @@ export class YouTube {
       });
     });
   }
+  async autoplay(song: Song) {
+    return new Promise<Song | null>(async (res, rej) => {
+      const websiteData = await fetch(
+        song.url
+      );
+      const website = await websiteData.text();
+        const $ = cheerio.load(website);
+        const result = $('body').find('#content').find('.related-list-item').find('a').attr('href');
+        const url = `https://www.youtube.com${result}`;
+        const nextSong = await this.getInformation(url);
+        res(nextSong);
+    })
+    
+  }
 }
