66d31de1cf7e42669cfa01aef92413f87b7c1403
KaindlJulian <kaindl745@gmail.com>
2019-12-05 19:05:42 +0100

    refactor(core): Replace pseudo subcommands with real ones
    This mainly means the interface can be more flexible:
    
    Allowing the parent command to
    have arguments, which was not possible before.
    
    Also this means subcommands of different parents
    can have the same name if needed.

--- src/core/handlers/SocketCommandHandler.ts
+++ src/core/handlers/SocketCommandHandler.ts
@@ -28,15 +28,13 @@ export class SocketCommandHandler extends BotHandler {
     }
 
     try {
-      // maybe add a default text channel feature to the bot later
-      // which the user can set per command and we would take that channel
-      // for now we take the first text channel
       const message = this.createMessage(socketCommand.messageData.guildId);
       command.execute(message, socketCommand.args);
     } catch (error) {
       console.log(error);
     }
   }
+
   createMessage(guildId: string) {
     const guild = this.bot.guilds.filter(g => g.id === guildId).first();
     const textChannel = guild.channels
--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -3,5 +3,7 @@ import { CommandOptions } from './CommandOptions';
 
 export interface Command {
   options: CommandOptions;
+  subCommands?: Command[];
+
   execute(msg: Message, args: string[]): void;
 }
