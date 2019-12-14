94016d426f4e160506bcf2a27f79ed1eb5bcd057
KaindlJulian <kaindl745@gmail.com>
2019-12-14 06:12:03 +0100

    feat(help): Support for help with arguments
    - Enables usage like "?help play"
    - Reformat output of general help

--- src/commands/Help.ts
+++ src/commands/Help.ts
@@ -1,6 +1,6 @@
 import { Message, RichEmbed } from 'discord.js';
 import { Command } from '../interfaces/Command';
-import { BOT } from '../config';
+import { BOT, COLOR } from '../config';
 import { bot } from '../bot';
 
 export class Help implements Command {
@@ -15,18 +15,70 @@ export class Help implements Command {
   };
 
   execute(msg: Message, args: string[]): void {
-    let helpString = '';
-    bot.commands.forEach(command => {
-      helpString += `\`\`${BOT.SERVERPREFIXES[msg.guild.id]}${
-        command.options.displayName
-      } - ${command.options.description}\`\` \n`;
-    });
-    const embed = new RichEmbed()
-      .setTitle('Available Commands')
-      .setColor('#00e773')
-      .setDescription('All Available Commands are listed below')
-      .addBlankField()
-      .addField('Commands', helpString);
-    msg.channel.send(embed);
+    const embed = new RichEmbed().setColor(COLOR.CYAN);
+
+    if (args[0]) {
+      const command = bot.findCommand(args[0]);
+
+      if (!command) {
+        msg.reply('Not found');
+        return;
+      }
+
+      embed.setTitle(command.options.id.name);
+      embed.setDescription(command.options.description);
+
+      embed.addBlankField();
+
+      embed.addField(
+        'Aliases',
+        command.options.id.aliases?.map(a => `• ${a}`).join('\n') + '\n',
+        true
+      );
+
+      embed.addBlankField(true);
+
+      embed.addField(
+        'Usage',
+        `\`${BOT.SERVERPREFIXES[msg.guild.id]}${command.options.displayName}\``,
+        true
+      );
+
+      msg.channel.send(embed);
+    } else {
+      embed.setTitle('Available Commands');
+      embed.setDescription(
+        `All Commands are listed below. Use \`${
+          BOT.SERVERPREFIXES[msg.guild.id]
+        } help [command]\` to see more details on a single command`
+      );
+
+      // Discord limits fields in embeds to 25
+      let embedCount = 0;
+
+      const fieldBuffer = new Array<{
+        name: string;
+        value: string;
+        inline?: boolean;
+      }>();
+
+      bot.commands.forEach(command => {
+        fieldBuffer.push({
+          name: command.options.id.name,
+          value: `${command.options.description}\n\`${
+            BOT.SERVERPREFIXES[msg.guild.id]
+          }${command.options.displayName}\``,
+        });
+      });
+
+      while (fieldBuffer.length > 0) {
+        fieldBuffer.splice(embedCount * 25, 25).forEach(field => {
+          embed.addField(field.name, field.value, field.inline);
+        });
+        msg.channel.send(embed);
+        embed.fields = [];
+        embedCount++;
+      }
+    }
   }
 }
