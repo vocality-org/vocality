3b145082ba19f1c62128d1580166d2ba1cb26e8e
KaindlJulian <kaindl745@gmail.com>
2019-12-14 16:07:17 +0100

    feat(volume): Add relative volume changes
    Usable like:
    "?volume [up/down]" (up/down by 0.1)
    "?volume [-/+][number]"

--- src/commands/Volume.ts
+++ src/commands/Volume.ts
@@ -1,8 +1,11 @@
+import { Message } from 'discord.js';
 import { Command } from '../interfaces/Command';
 import { CommandOptions } from '../interfaces/CommandOptions';
-import { Message } from 'discord.js';
 import { ServerQueueController } from '../core/ServerQueueController';
 import { onVolumeChange } from '../dashboard-ws';
+
+const UP_VALUE = 10;
+const DOWN_VALUE = -10;
 
 export class Volume implements Command {
   options: CommandOptions = {
@@ -11,22 +14,63 @@ export class Volume implements Command {
       aliases: ['vol'],
     },
     minArguments: 1,
-    displayName: 'volume',
-    description: 'change the Volume of the bot (0-100)',
+    displayName: 'volume [number or up/down]',
+    description: 'Change the volume of the bot with a number or keyword',
     socketEnabled: true,
   };
   execute(msg: Message, args: string[]): void {
-    if (isNaN(+args[0])) {
-      msg.reply('Volume is not a number');
+    if (!this.validateArguments(args)) {
+      msg.reply('Volume is not valid');
       return;
     }
+
     const serverEntry = ServerQueueController.getInstance().find(msg.guild.id)!;
-    const vol = Number.parseFloat(args[0]);
-    serverEntry.volume = vol / 100;
+    const vol = this.calculateVolume(args[0], serverEntry.volume);
+    serverEntry.volume = vol;
+
     if (serverEntry.connection) {
       serverEntry.connection!.dispatcher.setVolume(vol / 100);
     }
+
     msg.reply(`Volume changed to ${vol}`);
     onVolumeChange(vol);
+  }
+
+  validateArguments(args: string[]): boolean {
+    return (
+      !isNaN(+args[0]) ||
+      args[0].toLowerCase() === 'up' ||
+      args[0].toLowerCase() === 'down' ||
+      (args[0].match('\\^[+-]d{1,2}') ? true : false) // eg -10 or +5
+    );
+  }
+
+  calculateVolume(arg0: string, volume: number): number {
+    let relativeVolume: number | undefined;
+
+    if (arg0.startsWith('-')) {
+      relativeVolume = volume - parseFloat(arg0.substr(1));
+    }
+    if (arg0.startsWith('+')) {
+      relativeVolume = volume + parseFloat(arg0.substr(1));
+    }
+    if (arg0.toLowerCase() === 'up') {
+      relativeVolume = volume + UP_VALUE;
+    }
+    if (arg0.toLowerCase() === 'down') {
+      relativeVolume = volume - DOWN_VALUE;
+    }
+
+    if (relativeVolume) {
+      if (relativeVolume < 0) {
+        return 0;
+      }
+      if (relativeVolume > 100) {
+        return 100;
+      }
+      return relativeVolume;
+    }
+
+    return parseFloat(arg0);
   }
 }
