96e4c7253753b5031d478cc3b88c365ca4098f88
Julian <kaindl745@gmail.com>
2020-01-06 09:19:53 +0100

    feat(core): Add ChangePrefix command

---
+++ packages/vocality-core/src/commands/ChangePrefix.ts
@@ -0,0 +1,21 @@
+import { Command, CommandOptions } from '@vocality-org/types';
+import { Message } from 'discord.js';
+import { BOT } from '../config';
+
+export class ChangePrefix implements Command {
+  options: CommandOptions = {
+    id: {
+      name: 'prefix',
+    },
+    displayName: 'prefix <character>',
+    description: 'Change the bot message prefix for this server',
+    minArguments: 1,
+  };
+
+  execute(msg: Message, args: string[]) {
+    BOT.SERVERPREFIXES[msg.guild.id] = args[0];
+    msg.channel.send(
+      `Prefix changed to \`${BOT.SERVERPREFIXES[msg.guild.id]}\``
+    );
+  }
+}
