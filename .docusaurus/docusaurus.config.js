95ada5ccf0991a0c96b428d4b07bcbd019f51676
Julian <kaindl745@gmail.com>
2020-01-12 20:33:26 +0100

    ci(core): Add index.js

--- .docusaurus/docusaurus.config.js
+++ .docusaurus/docusaurus.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Vocality Documentation',
-  tagline: 'Documentation for the vocality core package',
+  tagline: 'The plugin based discord bot',
   url: 'https://github.com/vocality-org/vocality',
   baseUrl: '/vocality/',
   favicon: 'img/favicon.ico',
@@ -9,6 +9,10 @@ module.exports = {
   themeConfig: {
     navbar: {
       title: 'Vocality Documentation',
+      logo: {
+        alt: 'Logo',
+        src: 'img/logo.svg',
+      },
       links: [
         { to: 'docs/globals', label: 'Docs', position: 'left' },
         {
@@ -23,6 +27,9 @@ module.exports = {
     [
       '@docusaurus/preset-classic',
       {
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
         },
---
+++ .docusaurus/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: <>Plugins</>,
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        Vocality offers support for Plugins. Either add official plugins or
+        provide your own.
+      </>
+    ),
+  },
+  {
+    title: <>Focus on What Matters</>,
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    description: (
+      <>
+        Docusaurus lets you focus on the implementation of your Plugin or
+        Commands.
+      </>
+    ),
+  },
+  {
+    title: <>Built for performance</>,
+    imageUrl: 'img/undraw_docusaurus_react.svg',
+    description: (
+      <>
+        By utilizing the discord.js library great performance can be achieved.
+      </>
+    ),
+  },
+];
+
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={classnames('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Plugin based discord bot"
+    >
+      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={classnames(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted
+              )}
+              to={useBaseUrl('docs/gobals')}
+            >
+              To Documentation
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
