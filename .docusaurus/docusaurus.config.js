36b235cc82f46834c1465dcc51cac6b85f3f7eb5
Julian <kaindl745@gmail.com>
2020-01-12 19:32:38 +0100

    ci(core): Add docusaurus config

---
+++ .docusaurus/docusaurus.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: 'Vocality Documentation',
+  tagline: 'Documentation for the vocality core package',
+  url: 'https://github.com/vocality-org/vocality',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'vocality-org',
+  projectName: 'vocality',
+  themeConfig: {
+    navbar: {
+      title: 'Vocality Documentation',
+      links: [
+        { to: 'docs/globals', label: 'Docs', position: 'left' },
+        {
+          href: 'https://github.com/vocality-org/vocality',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+      },
+    ],
+  ],
+};
